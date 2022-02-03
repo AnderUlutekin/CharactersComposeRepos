2b3edaa550d7ba3460011cef481f4c400324acc7
ander <ander.ulutekin@gmail.com>
2022-02-03 21:33:15 +0300

    MVVM

---
+++ app/src/main/java/com/example/composedemo/model/Character.kt
@@ -0,0 +1,3 @@
+package com.example.composedemo.model
+
+data class Character(val image: Int, val name: String)
---
+++ app/src/main/java/com/example/composedemo/model/characterList.kt
@@ -0,0 +1,11 @@
+package com.example.composedemo
+
+import com.example.composedemo.model.Character
+
+val characterList = listOf<Character>(
+    Character(R.drawable.sponge_bob, "Sponge Bob"),
+    Character(R.drawable.timmy_turner, "Timmy Turner"),
+    Character(R.drawable.phineas_flynn, "Phineas Flynn"),
+    Character(R.drawable.kenny_mccormick, "Kenny McCormick"),
+    Character(R.drawable.tasmanian_devil, "Tasmanian Devil"),
+)
